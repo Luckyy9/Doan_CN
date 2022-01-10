a105c6b5cf8de1020f5bcd33fc2abd2939e4acfd
hoavo1911 <vominhhoa19112000.dx.bp@gmail.com>
2022-01-10 21:30:26 +0700

    completed news page, add search function, fix something

--- src/app/models/News.js
+++ src/app/models/News.js
@@ -5,15 +5,9 @@ const ObjectId = Schema.ObjectId;
 
 const News = new Schema({
  name:{type:String, required: true},
- titile:{type:String, required: true},
- imformation:{type:String},
- slug:{type:String},
- type:{type:String},
- img:{type:String},
- like:{type:String},
- comment:{type:String},
- share:{type:String},
- by:{type:String},
+ description: {type:String},
+ image:{type:String},
+ slug:{type:String}
 },{
     timestamps:true,
 });
